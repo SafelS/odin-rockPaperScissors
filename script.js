f38c7c8138d00b9d9ae23293f0dd9762b8079722
SafelS <134007234+SafelS@users.noreply.github.com>
2025-09-27 00:05:00 +0300

    finished the project removed whoBegins() because you only play against computer

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-console.log("Script connected");
+//console.log("Script connected");
 
-function whoBegins(){
-    let beginner = Math.random();
-    if(beginner == 0){
-        console.log("User beginnt");
-        return 0;
-    }else{
-        console.log("Computer beginnt");
-        return 1;
-    }
-}
+let humanScore = 0;
+let computerScore = 0;
+
+
+/*
+0 == rock
+1 == paper
+2 == scissors
+*/
 
 function getComputerChoice(){
     return Math.floor(Math.random() * 3);
@@ -18,3 +17,58 @@ function getComputerChoice(){
 console.log(getComputerChoice())
 
 
+function getHumanChoice(){
+    let choice = prompt("rock, paper or scissors?");
+    if(choice.toLowerCase() == "rock"){
+        return 0;
+    }else if(choice.toLowerCase() == "paper"){
+        return 1;
+    }else{
+        return 2;
+    }
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Draw!");
+    } 
+    else if (
+        (humanChoice === 0 && computerChoice === 2) || // rock beats scissors
+        (humanChoice === 1 && computerChoice === 0) || // paper beats stone
+        (humanChoice === 2 && computerChoice === 1)    // scissors beats paper
+    ) {
+        console.log("You won!");
+        humanScore++;
+        return humanScore
+    } 
+    else {
+        console.log("Computer won!");
+        computerScore++;
+        return computerScore
+    }
+}
+
+
+
+//console.log(humanScore)
+//console.log(computerScore)
+
+function playGame(){
+    for(let i = 0; i<5; i++ ){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    if(humanScore == computerScore){
+        console.log("Nobody won, we'll get 'em next time")
+    }else if(humanScore > computerScore){
+        console.log("You Won The Match with " + humanScore + " points");
+    }else{
+        console.log("The Clankers won. The scored " + computerScore + " points");
+    }
+}
+
+playGame();
