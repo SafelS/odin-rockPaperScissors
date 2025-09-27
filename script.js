eec1e546c5c8b9b062a4cd2e7a2c0251e434fc8c
SafelS <134007234+SafelS@users.noreply.github.com>
2025-09-27 17:28:35 +0300

    Not finished

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-//console.log("Script connected");
-
 let humanScore = 0;
 let computerScore = 0;
+let choice = -1;
 
+let resultDiv = document.querySelector(".result-container");
+
+
+
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
 
 /*
 0 == rock
@@ -10,65 +16,50 @@ let computerScore = 0;
 2 == scissors
 */
 
-function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
-}
 
-console.log(getComputerChoice())
+const btn = document.querySelector(".buttons");
 
+btn.addEventListener("click", (event) => {
+    let target = event.target;
 
-function getHumanChoice(){
-    let choice = prompt("rock, paper or scissors?");
-    if(choice.toLowerCase() == "rock"){
-        return 0;
-    }else if(choice.toLowerCase() == "paper"){
-        return 1;
-    }else{
-        return 2;
+    if(target.classList.contains("rock")){
+        playRound(0,getComputerChoice());
+    }else if(target.classList.contains("paper")){
+        playRound(1, getComputerChoice());
+    }else if(target.classList.contains("scissors")){
+        playRound(2, getComputerChoice());
     }
-}
+});
 
 
 function playRound(humanChoice, computerChoice) {
+    
+    if(humanScore == 5){
+        resultDiv.textContent = "Humans WON!";
+        humanScore = 0;
+    }else if(computerScore == 5){
+        resultDiv.textContent= "Clankers won....";
+        computerScore = 0;
+    }
+    
     if (humanChoice === computerChoice) {
-        console.log("Draw!");
+        //alert("Draw!");
+        resultDiv.textContent = "Draw!";
     } 
     else if (
         (humanChoice === 0 && computerChoice === 2) || // rock beats scissors
         (humanChoice === 1 && computerChoice === 0) || // paper beats stone
         (humanChoice === 2 && computerChoice === 1)    // scissors beats paper
     ) {
-        console.log("You won!");
+        resultDiv.textContent = "You Won"
         humanScore++;
-        return humanScore
+        //return humanScore
     } 
     else {
-        console.log("Computer won!");
+        resultDiv.textContent = "Computer Won"
         computerScore++;
-        return computerScore
+        //return computerScore
     }
+
+
 }
-
-
-
-//console.log(humanScore)
-//console.log(computerScore)
-
-function playGame(){
-    for(let i = 0; i<5; i++ ){
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-    if(humanScore == computerScore){
-        console.log("Nobody won, we'll get 'em next time")
-    }else if(humanScore > computerScore){
-        console.log("You Won The Match with " + humanScore + " points");
-    }else{
-        console.log("The Clankers won. The scored " + computerScore + " points");
-    }
-}
-
-playGame();
